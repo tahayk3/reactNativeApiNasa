53007720bd6974e371bcd0ff553ca98d0fcd7b06
tahayk3 <tahayk3@gmail.com>
2024-08-08 10:06:24 -0600

    agregando useEffect para cargar la api

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, StyleSheet} from 'react-native';
 import Header from '../../components/Header';
+import fetchApi from '../../utils/fetch';
 
 const Home = () => {
+  /*constante para manejar el estado, en este caso, se maneja de manera local(no hacia otros componentes), de ser
+   el caso, podria implementarse redux
+  */
+  const [todaysImage, setTodayImage] = useState();
+  //useEffect para cargar y recargar api
+  useEffect(() => {
+    //metodo para llamar api
+    const loadTodayImage = async () => {
+      try {
+        const todaysImageResponse = await fetchApi();
+        setTodayImage(todaysImageResponse);
+      } catch (error) {
+        console.error(error);
+        setTodayImage(undefined);
+      }
+    };
+    //llamada a la funcion y se captura el error
+    loadTodayImage().catch(null);
+  }, []);
+
+  console.log(todaysImage);
+
   return (
     <View style={styles.container}>
       <Header></Header>
